12bf1e38cc1b059ab01143f13a689aafbac0c011
Elnata Eleale <elnatan.des@gmail.com>
2017-10-23 17:27:55 +0000

    Criando helpers

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,7 +1,7 @@
 module RecipesHelper
     def formata_caloria(recipe)
         if recipe.light?
-            content_tag(:strong, "Light (Menos de 100 calorias")
+            content_tag(:strong, "Light (Menos de 100 calorias)")
         else
             recipe.calories
         end
